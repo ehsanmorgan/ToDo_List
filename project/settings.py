5ccf0cd38a30d1653e9874a0ab3b228b4de10400
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-06-20 21:35:05 +0200

    .

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-
     'todo_list',
    
 ]
@@ -128,3 +127,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+ 
+    # ...
+]
